eb9bd6622ec4942b69a8b728c7198e3563ffc86b
winniexuwen <winniexuwen@hotmail.com>
2017-01-20 15:15:56 +0800

    android uikit 3.4.0

--- src/com/netease/nim/uikit/common/ui/recyclerview/entity/MultiItemEntity.java
+++
@@ -1,7 +0,0 @@
-package com.netease.nim.uikit.common.ui.recyclerview.entity;
-
-public interface MultiItemEntity {
-
-    int getItemType();
-
-}
--- src/com/netease/nim/uikit/session/emoji/IEmoticonSelectedListener.java
+++
@@ -1,7 +0,0 @@
-package com.netease.nim.uikit.session.emoji;
-
-public interface IEmoticonSelectedListener {
-	void onEmojiSelected(String key);
-
-	void onStickerSelected(String categoryName, String stickerName);
-}
